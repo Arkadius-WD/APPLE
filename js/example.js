a1d29ffcfd3367505ed3bbc4bcff575b0f462cbc
Arkadius-WD <arkadiusz.kamyczek@gmail.com>
2022-11-23 10:23:23 +0100

    Slider info top & bottom html upgrade

--- js/example.js
+++ js/example.js
@@ -1,3 +1,42 @@
+//////////////////////// NAV ////////////////////////////
+/////////////////////////////////////////////////////////
+const navAnimation = () => {
+	const topNav = document.querySelector('.nav-top')
+	const navBtn = document.querySelector('.nav-bar__burger')
+	const navBag = document.querySelector('.nav-bar__bag')
+	const allNavItems = document.querySelectorAll('.nav-top__item')
+
+	const showMenu = () => {
+		topNav.classList.toggle('nav-hidden')
+		navBag.classList.toggle('bag-hidden')
+		navBtn.classList.toggle('click-btn')
+
+		allNavItems.forEach(item => {
+			item.addEventListener('click', () => {
+				topNav.classList.remove('nav-hidden')
+				navBag.classList.remove('bag-hidden')
+				navBtn.classList.remove('click-btn')
+			})
+		})
+
+		handleNavItemsAnimation()
+	}
+
+	const handleNavItemsAnimation = () => {
+		let delayTime = 0
+		allNavItems.forEach(item => {
+			item.classList.toggle('nav-item-animation')
+			item.style.animationDelay = delayTime / 15 + 's'
+			delayTime++
+		})
+	}
+
+	navBtn.addEventListener('click', showMenu)
+}
+navAnimation()
+
+////////////////////// SLIDER ///////////////////////////
+/////////////////////////////////////////////////////////
 const slider = () => {
 	const slides = document.querySelectorAll('.slider__slide')
 	const leftBtn = document.querySelector('.slider__previous')
@@ -25,9 +64,9 @@ const slider = () => {
 	}
 
 	const init = () => {
-		goToSLide(0)
+		goToSLide(1)
 		createDots()
-		activateDot(0)
+		activateDot(1)
 	}
 
 	// NEXT SLIDE
