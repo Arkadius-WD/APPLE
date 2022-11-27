d5af3c801fd7d9e3f67bddf06639488a6f97a28d
Arkadius-WD <arkadiusz.kamyczek@gmail.com>
2022-11-27 12:56:07 +0100

    Carousel wihout active style on slides

--- js/example.js
+++ js/example.js
@@ -37,124 +37,134 @@ navAnimation()
 
 ////////////////////// SLIDER ///////////////////////////
 /////////////////////////////////////////////////////////
+const autoSlider = () => {
+	const slideContainer = document.querySelector('.slider__track-container')
+	const slides = document.querySelector('.slider__slides')
+	const nextBtn = document.querySelector('.slider__next')
+	const prevBtn = document.querySelector('.slider__previous')
+	const dotContainer = document.querySelector('.slider__nav-dots-contaner')
+	const interval = 3000
 
-const slideContainer = document.querySelector('.slider__track-container')
-const slides = document.querySelector('.slider__slides')
-const nextBtn = document.querySelector('.slider__next')
-const prevBtn = document.querySelector('.slider__previous')
-const dotContainer = document.querySelector('.slider__nav-dots-contaner')
-const interval = 3000
+	let slide = document.querySelectorAll('.slider__slide')
+	let index = 1
+	let slideId
 
-let slide = document.querySelectorAll('.slider__slide')
-let index = 1
-let slideId
+	// CLONING SLIDE
+	const firstClone = slide[0].cloneNode(true)
+	const lastClone = slide[slide.length - 1].cloneNode(true)
 
-const firstClone = slide[0].cloneNode(true)
-const lastClone = slide[slide.length - 1].cloneNode(true)
+	firstClone.id = 'first-clone'
+	lastClone.id = 'last-clone'
 
-firstClone.id = 'first-clone'
-lastClone.id = 'last-clone'
+	slides.append(firstClone)
+	slides.prepend(lastClone)
 
-slides.append(firstClone)
-slides.prepend(lastClone)
+	const slideWidth = slide[index].clientWidth
 
-const slideWidth = slide[index].clientWidth
-
-slides.style.transform = `translateX(${-slideWidth * index}px)`
-
-const getSlides = () => document.querySelectorAll('.slider__slide')
-
-slides.addEventListener('transitionend', () => {
-	slide = getSlides()
-	if (slide[index].id === firstClone.id) {
-		slides.style.transition = 'none'
-		index = 1
-		slides.style.transform = `translate(${-slideWidth * index}px)`
-	}
-
-	if (slide[index].id === lastClone.id) {
-		slides.style.transition = 'none'
-		index = slide.length - 2
-		slides.style.transform = `translate(${-slideWidth * index}px)`
-	}
-})
-
-const createDots = () => {
-	slide.forEach((_, i) => {
-		dotContainer.insertAdjacentHTML('beforeend', `<button class="slider__nav-dot"data-slide="${i + 1}"></button>`)
-	})
-}
-createDots()
-
-const activateDot = slide => {
-	document.querySelectorAll('.slider__nav-dot').forEach(dot => dot.classList.remove('slider__nav-dot--active'))
-
-	document.querySelector(`.slider__nav-dot[data-slide="${slide}"]`).classList.add('slider__nav-dot--active')
-}
-activateDot(1)
-
-const goToSLide = slide => {
-	slides.style.transform = `translate(${-slideWidth * slide}px)`
-}
-
-const startSlide = () => {
-	slideId = setInterval(() => {
-		moveToNextSlide()
-	}, interval)
-}
-startSlide()
-
-// NEXT SLIDE
-const moveToNextSlide = () => {
-	if (index >= slide.length - 1) {
-		return
-	} else {
-		index++
-	}
-	slides.style.transition = '.7s ease-out'
-	slides.style.transform = `translate(${-slideWidth * index}px)`
-
-	if (index >= slide.length - 1) {
-		activateDot(1)
-	} else {
-		activateDot(index)
-	}
-}
-
-// PREV SLIDE
-const moveToPreviousSlide = () => {
-	if (index <= 0) return
-	index--
-	slides.style.transition = '.7s ease-out'
 	slides.style.transform = `translateX(${-slideWidth * index}px)`
 
-	if (index <= 0) {
-		activateDot(slide.length - 2)
-	} else {
-		activateDot(index)
+	const getSlides = () => document.querySelectorAll('.slider__slide')
+
+	slides.addEventListener('transitionend', () => {
+		slide = getSlides()
+		if (slide[index].id === firstClone.id) {
+			slides.style.transition = 'none'
+			index = 1
+			slides.style.transform = `translate(${-slideWidth * index}px)`
+		}
+
+		if (slide[index].id === lastClone.id) {
+			slides.style.transition = 'none'
+			index = slide.length - 2
+			slides.style.transform = `translate(${-slideWidth * index}px)`
+		}
+	})
+
+	// FUNCTIONS
+	const createDots = () => {
+		slide.forEach((_, i) => {
+			dotContainer.insertAdjacentHTML('beforeend', `<button class="slider__nav-dot"data-slide="${i + 1}"></button>`)
+		})
 	}
+
+	const activateDot = slide => {
+		document.querySelectorAll('.slider__nav-dot').forEach(dot => dot.classList.remove('slider__nav-dot--active'))
+
+		document.querySelector(`.slider__nav-dot[data-slide="${slide}"]`).classList.add('slider__nav-dot--active')
+	}
+
+	const goToSLide = slide => {
+		slides.style.transform = `translate(${-slideWidth * slide}px)`
+	}
+
+	const startSlide = () => {
+		slideId = setInterval(() => {
+			moveToNextSlide()
+		}, interval)
+	}
+
+	const init = () => {
+		createDots()
+		goToSLide(1)
+		activateDot(1)
+		startSlide()
+	}
+
+	// NEXT SLIDE
+	const moveToNextSlide = () => {
+		if (index >= slide.length - 1) {
+			return
+		} else {
+			index++
+		}
+		slides.style.transition = '.7s ease-out'
+		slides.style.transform = `translate(${-slideWidth * index}px)`
+
+		if (index >= slide.length - 1) {
+			activateDot(1)
+		} else {
+			activateDot(index)
+		}
+	}
+
+	// PREV SLIDE
+	const moveToPreviousSlide = () => {
+		if (index <= 0) return
+		index--
+		slides.style.transition = '.7s ease-out'
+		slides.style.transform = `translateX(${-slideWidth * index}px)`
+
+		if (index <= 0) {
+			activateDot(slide.length - 2)
+		} else {
+			activateDot(index)
+		}
+	}
+
+	init()
+
+	// EVENT HANDLERS
+	slideContainer.addEventListener('mouseenter', () => {
+		clearInterval(slideId)
+	})
+
+	slideContainer.addEventListener('mouseleave', startSlide)
+	nextBtn.addEventListener('click', moveToNextSlide)
+	prevBtn.addEventListener('click', moveToPreviousSlide)
+
+	document.addEventListener('keydown', e => {
+		e.key === 'ArrowLeft' && moveToPreviousSlide()
+		e.key === 'ArrowRight' && moveToNextSlide()
+	})
+
+	dotContainer.addEventListener('click', e => {
+		if (e.target.classList.contains('slider__nav-dot')) {
+			const { slide } = e.target.dataset
+			goToSLide(slide)
+			activateDot(slide)
+
+			console.log(e.target)
+		}
+	})
 }
-
-// EVENT HANDLERS
-slideContainer.addEventListener('mouseenter', () => {
-	clearInterval(slideId)
-})
-
-slideContainer.addEventListener('mouseleave', startSlide)
-nextBtn.addEventListener('click', moveToNextSlide)
-prevBtn.addEventListener('click', moveToPreviousSlide)
-
-document.addEventListener('keydown', e => {
-	e.key === 'ArrowLeft' && moveToPreviousSlide()
-	e.key === 'ArrowRight' && moveToNextSlide()
-})
-
-dotContainer.addEventListener('click', e => {
-	if (e.target.classList.contains('slider__nav-dot')) {
-		const { slide } = e.target.dataset
-		goToSLide(slide)
-		activateDot(slide)
-
-		console.log(e.target)
-	}
-})
+autoSlider()
