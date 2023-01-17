645eceacad6da3473553265733a85f6d596ffae5
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-01-17 19:29:08 +0100

    bagMessage modifiction

--- js/nav.js
+++ js/nav.js
@@ -4,6 +4,7 @@ export const navAnimation = () => {
 	const navBar = document.querySelector('.nav-bar')
 	const navBtn = document.querySelector('.nav-bar__burger')
 	const navBag = document.querySelector('.nav-bar__bag')
+	const navBagMessage = document.querySelector('.bag-message')
 	const navBarClose = document.querySelector('.nav-bar__input-close')
 	const navSearch = document.querySelector('.nav-bar__search')
 	const allNavItems = document.querySelectorAll('.nav-top__item')
@@ -11,12 +12,14 @@ export const navAnimation = () => {
 	const showMenu = () => {
 		topNav.classList.toggle('nav-hidden')
 		navBag.classList.toggle('bag-hidden')
+		navBagMessage.classList.toggle('bag-hidden')
 		navBtn.classList.toggle('click-btn')
 
 		allNavItems.forEach(item => {
 			item.addEventListener('click', () => {
 				topNav.classList.remove('nav-hidden')
 				navBag.classList.remove('bag-hidden')
+				navBagMessage.classList.remove('bag-hidden')
 				navBtn.classList.remove('click-btn')
 			})
 		})
