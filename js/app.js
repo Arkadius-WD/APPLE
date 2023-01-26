4bdb713d5a701b990e63207c3e7a59bd524048f1
Arkadius-WD <arkadiusz.kamyczek@gmail.com>
2023-01-26 11:04:09 +0100

    Store slider modifiation

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@ import { navAnimation } from './nav.js'
 import { autoSlider } from './slider.js'
 import { footerList } from './footer.js'
 import { storeApp } from './store.js'
-import { storeSlides } from './storeSlides.js'
 
 navAnimation()
 autoSlider()
 footerList()
-storeSlides()
 storeApp()
--- js/storeSlides.js
+++ js/storeSlides.js
@@ -1,18 +1,104 @@
 'use strict'
 
 export const storeSlides = () => {
-	const container = document.querySelector('.carousel-store__container')
-	const slides = Array.from(container.children)
-	const nextButton = document.querySelector('.carousel-store__next')
-	const prevButton = document.querySelector('.carousel-store__previous')
+	const carouselSlider = () => {
+		const container = document.querySelector('.carousel-store__container')
+		const slides = document.querySelectorAll('.carousel-store__card')
+		const nextButton = document.querySelector('.carousel-store__next')
+		const prevButton = document.querySelector('.carousel-store__previous')
 
-	const slideWidth = slides[0].getBoundingClientRect().width
+		let index = 0
+		prevButton.style.display = 'none'
 
-	nextButton.addEventListener('click', () => {
-		container.style.transform = `translateX(-${slideWidth}px)`
-	})
+		const slideWidth = slides[0].getBoundingClientRect().width
+		const containerWidth = container.getBoundingClientRect().width
+		const shiftSlide = slides.length - Math.round(containerWidth / slideWidth)
 
-	prevButton.addEventListener('click', () => {
-		container.style.transform = `translateX(${slideWidth}px)`
-	})
+		const moveToNextSlide = () => {
+			if (index >= shiftSlide - 1) {
+				nextButton.style.display = 'none'
+			}
+
+			if (index >= shiftSlide) {
+				return
+			} else {
+				index++
+				prevButton.style.display = 'block'
+			}
+			container.style.transition = 'transform 1s'
+			container.style.transform = `translateX(-${slideWidth * index}px)`
+		}
+
+		const moveToPreviousSlide = () => {
+			if (index <= 0 + 1) {
+				prevButton.style.display = 'none'
+			}
+
+			if (index <= 0) {
+				return
+			} else {
+				index--
+				nextButton.style.display = 'block'
+			}
+
+			container.style.transition = 'transform 1s'
+			container.style.transform = `translateX(${-slideWidth * index}px)`
+		}
+
+		nextButton.addEventListener('click', moveToNextSlide)
+		prevButton.addEventListener('click', moveToPreviousSlide)
+	}
+
+	const iPhoneSlider = () => {
+		const container = document.querySelector('.iPhone-store__devices-cards')
+		const slides = document.querySelectorAll('.iPhone-store__devices-card')
+		const nextButton = document.querySelector('.iPhone-store__next')
+		const prevButton = document.querySelector('.iPhone-store__previous')
+
+		let index = 0
+		prevButton.style.display = 'none'
+
+		const slideWidth = slides[0].getBoundingClientRect().width
+		const containerWidth = container.getBoundingClientRect().width
+		const shiftSlide = slides.length - Math.round(containerWidth / slideWidth)
+
+		const moveToNextSlide = () => {
+			if (index >= shiftSlide - 1) {
+				nextButton.style.display = 'none'
+			}
+
+			if (index >= shiftSlide) {
+				return
+			} else {
+				index++
+				prevButton.style.display = 'block'
+			}
+			container.style.transition = 'transform 1s'
+			container.style.transform = `translateX(-${slideWidth * index}px)`
+		}
+
+		const moveToPreviousSlide = () => {
+			if (index <= 0 + 1) {
+				prevButton.style.display = 'none'
+			}
+
+			if (index <= 0) {
+				return
+			} else {
+				index--
+				nextButton.style.display = 'block'
+			}
+
+			container.style.transition = 'transform 1s'
+			container.style.transform = `translateX(${-slideWidth * index}px)`
+		}
+
+		nextButton.addEventListener('click', moveToNextSlide)
+		prevButton.addEventListener('click', moveToPreviousSlide)
+
+		console.log(slideWidth)
+	}
+
+	carouselSlider()
+	iPhoneSlider()
 }
