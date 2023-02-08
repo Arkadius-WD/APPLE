3b4d5e5f039abf2b954998a54a1d3a775395d240
Arkadius-WD <arkadiusz.kamyczek@gmail.com>
2023-02-08 12:22:02 +0100

    store App structure

--- js/store.js
+++ js/store.js
@@ -1,3 +1,36 @@
 'use strict'
 
-export const storeApp = () => {}
+export const storeApp = () => {
+	const productsDOM = document.querySelector('.products')
+	const cartItems = document.querySelector('.nav-bar__bag-amount')
+	const cartDOM = document.querySelector('.bag-cart')
+	const cartContent = document.querySelector('.bag-cart__content')
+	const cartTotal = document.querySelector('.bag-cart__total')
+	const clearCartBtn = document.querySelector('.bag-cart__clear-cart')
+
+	// CART
+	let cart = []
+
+	// GETTING THE PRODUCTS
+	class Products {
+		async getProducts() {
+			try {
+				let result = await fetch('products.json')
+				return
+			} catch (error) {
+				console.log(error)
+			}
+		}
+	}
+
+	// DISPLAY PRODUCTS
+	class UI {}
+
+	// LOXAL STORAGE
+	class Storage {}
+
+	document.addEventListener('DOMContentLoaded', () => {
+		const ui = new UI()
+		const products = new Products()
+	})
+}
