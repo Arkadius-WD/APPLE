0c293b596736cbac80170479dd56585821ce78de
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-02-11 23:21:40 +0100

    store

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 import { navAnimation } from './nav.js'
 import { autoSlider } from './slider.js'
 import { footerList } from './footer.js'
+import { storeApp } from './store.js'
 
 navAnimation()
 autoSlider()
 footerList()
+storeApp()
