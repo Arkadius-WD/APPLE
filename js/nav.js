710b2b2ccd7d7b3a32f35b2412fb9dc2217a2b13
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-02-18 12:07:32 +0100

    cart items

--- js/nav.js
+++ js/nav.js
@@ -3,9 +3,9 @@ export const navAnimation = () => {
 	const topNav = document.querySelector('.nav-top')
 	const navBar = document.querySelector('.nav-bar')
 	const navBtn = document.querySelector('.nav-bar__burger')
-	const navBag = document.querySelector('.nav-bar__bag')
-	const bagCart = document.querySelector('.bag-cart')
-	const bagClose = document.querySelector('.bag-cart__close')
+	// const navBag = document.querySelector('.nav-bar__bag')
+	// const bagCart = document.querySelector('.bag-cart')
+	// const bagClose = document.querySelector('.bag-cart__close')
 	const navBarClose = document.querySelector('.nav-bar__input-close')
 	const navSearch = document.querySelector('.nav-bar__search')
 	const allNavItems = document.querySelectorAll('.nav-top__item')
@@ -31,9 +31,9 @@ export const navAnimation = () => {
 		navBar.classList.toggle('nav-bar--active')
 	}
 
-	const showCart = () => {
-		bagCart.classList.toggle('cart-show')
-	}
+	// const showCart = () => {
+	// 	bagCart.classList.toggle('cart-show')
+	// }
 
 	const handleNavItemsAnimation = () => {
 		let delayTime = 0
@@ -47,6 +47,6 @@ export const navAnimation = () => {
 	navBtn.addEventListener('click', showMenu)
 	navSearch.addEventListener('click', showSearchInput)
 	navBarClose.addEventListener('click', showSearchInput)
-	navBag.addEventListener('click', showCart)
-	bagClose.addEventListener('click', showCart)
+	// navBag.addEventListener('click', showCart)
+	// bagClose.addEventListener('click', showCart)
 }
--- js/store.js
+++ js/store.js
@@ -6,13 +6,18 @@ export const storeApp = () => {
 	const cartContent = document.querySelector('.bag-cart__content')
 	const cartTotal = document.querySelector('.bag-cart__total')
 	const clearCartBtn = document.querySelector('.bag-cart__clear-cart')
+	const navBag = document.querySelector('.nav-bar__bag')
+	const bagClose = document.querySelector('.bag-cart__close')
 
 	// CART
 	let cart = []
 
+	// BUTTONS
+	let buttonsDOM = []
+
 	// GETTING THE PRODUCTS
 	class Products {
-		async getProducts() {
+		async getProduct() {
 			try {
 				let result = await fetch('products.json')
 				let data = await result.json()
@@ -34,7 +39,7 @@ export const storeApp = () => {
 	class UI {
 		getBagButtons() {
 			const buttons = [...document.querySelectorAll('.bag-btn')]
-
+			buttonsDOM = buttons
 			buttons.forEach((button, index) => {
 				button.setAttribute('data-id', index + 1)
 				let id = button.dataset.id
@@ -42,20 +47,81 @@ export const storeApp = () => {
 				if (inCart) {
 					button.innerText = 'In Cart'
 					button.disabled = true
-				} else {
-					button.addEventListener('click', event => {
-						event.target.innerText = 'In Cart'
-						event.target.disabled = true
-					})
 				}
+				button.addEventListener('click', event => {
+					event.target.innerText = 'In Cart'
+					event.target.disabled = true
+					// get product from product
+					let cartItem = { ...Storage.getProduct(id), amount: 1 }
+					// add product to the cart
+					cart = [...cart, cartItem]
+					// save cart in local storage
+					Storage.saveCart(cart)
+					// set cart values
+					this.setCartValues(cart)
+					// display cart item
+					this.addCartItem(cartItem)
+					// show the cart
+					this.showCart()
+				})
 			})
 		}
+		setCartValues(cart) {
+			let tempTotal = 0
+			let itemsTotal = 0
+			cart.map(item => {
+				tempTotal += item.price * item.amount
+				itemsTotal += item.amount
+			})
+			cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
+			cartItems.innerText = itemsTotal
+		}
+		addCartItem(item) {
+			const div = document.createElement('div')
+			div.classList.add('bag-cart__item')
+			div.innerHTML = `
+				<img src=${item.image} alt="product">
+				<div class="bag-cart__text">
+					<h4>${item.title}</h4>
+					<h5>$${item.price}</h5>
+				</div>
+				<div class="bag-cart__menu">
+					<div class="bag-cart__menu-up" data-id=${item.id}>
+						<img src="./img/buttons/iconmonstr-angel-up-thin.svg" alt="up arrow">
+					</div>
+					<p class="bag-cart__menu-amount">${item.amount}</p>
+					<div class="bag-cart__menu-down" data-id=${item.id}>
+						<img src="./img/buttons/iconmonstr-angel-down-thin.svg" alt="down arrow">
+					</div>
+				</div>
+				<div class="bag-cart__trash" data-id=${item.id}>
+					<img src="./img/buttons/iconmonstr-trash-can-thin.svg" alt="">
+				</div>`
+			cartContent.appendChild(div)
+		}
+		showCart() {
+			cartDOM.classList.add('cart-show')
+		}
 	}
+
+	const switchCart = () => {
+		cartDOM.classList.toggle('cart-show')
+	}
+
+	navBag.addEventListener('click', switchCart)
+	bagClose.addEventListener('click', switchCart)
 
 	// LOCAL STORAGE
 	class Storage {
 		static saveProducts(products) {
 			localStorage.setItem('products', JSON.stringify(products))
+		}
+		static getProduct(id) {
+			let products = JSON.parse(localStorage.getItem('products'))
+			return products.find(product => product.id === id)
+		}
+		static saveCart(cart) {
+			localStorage.setItem('cart', JSON.stringify(cart))
 		}
 	}
 
@@ -65,9 +131,8 @@ export const storeApp = () => {
 
 		// get all products
 		products
-			.getProducts()
+			.getProduct()
 			.then(products => {
-				console.log(products)
 				Storage.saveProducts(products)
 			})
 			.then(() => {
